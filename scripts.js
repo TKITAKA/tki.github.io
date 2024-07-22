59abea28b66d010ad606db83c7adf591a0c8f742
TKITAKA <d_sl@naver.com>
2024-07-22 14:53:30 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,92 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var slidersContainer = document.querySelector('.sliders-container');
+document.addEventListener("DOMContentLoaded", function() {
+  const pages = document.querySelectorAll('.page');
+  const inputs = document.querySelectorAll('input[name="trigger"]');
 
-    // Initializing the numbers slider
-    var msNumbers = new MomentumSlider({
-        el: slidersContainer,
-        cssClass: 'ms--numbers',
-        range: [1, 4],
-        rangeContent: function (i) {
-            return '0' + i;
-        },
-        style: {
-            transform: [{scale: [0.4, 1]}],
-            opacity: [0, 1]
-        },
-        interactive: false
+  inputs.forEach((input, index) => {
+    input.addEventListener('change', () => {
+      pages.forEach((page, pageIndex) => {
+        page.style.display = pageIndex === index ? 'block' : 'none';
+      });
     });
+  });
 
-    // Initializing the titles slider
-    var titles = [
-        'King of the Ring Fight',
-        'Sound of Streets',
-        'Urban Fashion',
-        'Windy Sunset'
-    ];
-    var msTitles = new MomentumSlider({
-        el: slidersContainer,
-        cssClass: 'ms--titles',
-        range: [0, 3],
-        rangeContent: function (i) {
-            return '<h3>'+ titles[i] +'</h3>';
-        },
-        vertical: true,
-        reverse: true,
-        style: {
-            opacity: [0, 1]
-        },
-        interactive: false
-    });
-
-    // Initializing the links slider
-    var msLinks = new MomentumSlider({
-        el: slidersContainer,
-        cssClass: 'ms--links',
-        range: [0, 3],
-        rangeContent: function () {
-            return '<a class="ms-slide__link">View Case</a>';
-        },
-        vertical: true,
-        interactive: false
-    });
-
-    // Get pagination items
-    var pagination = document.querySelector('.pagination');
-    var paginationItems = [].slice.call(pagination.children);
-
-    // Initializing the images slider
-    var msImages = new MomentumSlider({
-        // Element to append the slider
-        el: slidersContainer,
-        // CSS class to reference the slider
-        cssClass: 'ms--images',
-        // Generate the 4 slides required
-        range: [0, 3],
-        rangeContent: function () {
-            return '<div class="ms-slide__image-container"><div class="ms-slide__image"></div></div>';
-        },
-        // Syncronize the other sliders
-        sync: [msNumbers, msTitles, msLinks],
-        // Styles to interpolate as we move the slider
-        style: {
-            '.ms-slide__image': {
-                transform: [{scale: [1.5, 1]}]
-            }
-        },
-        // Update pagination if slider change
-        change: function(newIndex, oldIndex) {
-            if (typeof oldIndex !== 'undefined') {
-                paginationItems[oldIndex].classList.remove('pagination__item--active');
-            }
-            paginationItems[newIndex].classList.add('pagination__item--active');
-        }
-    });
-
-    // Select corresponding slider item when a pagination button is clicked
-    pagination.addEventListener('click', function(e) {
-        if (e.target.matches('.pagination__button')) {
-            var index = paginationItems.indexOf(e.target.parentNode);
-            msImages.select(index);
-        }
-    });
+  // 기본적으로 첫 페이지를 표시합니다.
+  if (inputs.length > 0) {
+    inputs[0].checked = true;
+    pages[0].style.display = 'block';
+  }
 });
