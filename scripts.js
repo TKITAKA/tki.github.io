d7a0333cd6f364ee1e0f19631a7e4dd345d11641
TKITAKA <d_sl@naver.com>
2024-07-22 14:57:33 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const pages = document.querySelectorAll('.page');
-  const inputs = document.querySelectorAll('input[name="trigger"]');
-
-  inputs.forEach((input, index) => {
-    input.addEventListener('change', () => {
-      pages.forEach((page, pageIndex) => {
-        page.style.display = pageIndex === index ? 'block' : 'none';
-      });
-    });
-  });
-
-  // 기본적으로 첫 페이지를 표시합니다.
-  if (inputs.length > 0) {
-    inputs[0].checked = true;
-    pages[0].style.display = 'block';
-  }
-});
+//여백의 미
